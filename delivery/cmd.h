7a5006c1785b227b694b263cacc76de6209e17af
Daniel Junho <djunho@student.42porto.com>
2025-04-21 19:34:04 +0100

    Create the first version of the command struct

---
+++ delivery/cmd.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djunho <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 11:23:29 by djunho            #+#    #+#             */
+/*   Updated: 2025/04/21 11:46:21 by djunho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef CMD_H
+# define CMD_H
+
+# include "libft/libft.h"
+
+typedef enum e_node_op
+{
+	OP_AND,
+	OP_OR,
+	OP_PIPE,
+	OP_CMD,
+}	t_node_op;
+
+typedef struct s_cmd
+{
+	t_list	tokens;		// cmd followed by args
+}	t_cmd;
+
+typedef struct s_content_node
+{
+	t_node_op	op;
+	t_cmd		cmd;
+}	t_content_node;
+
+#endif // CMD_H
