b22851023990713399d1eb11b7942217fedcdeeb
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2025-06-27 18:57:23 +0100

    remove comment

--- delivery/execute/env_utils.c
+++ delivery/execute/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:57:47 by djunho            #+#    #+#             */
-/*   Updated: 2025/06/27 18:32:50 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/06/27 18:56:59 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 #include "../parser/parser.h"
 
 // Search for the environment variable at the var_list
-//   env didn't contains the = at the end, we compare with len + 1
-//   because we need have sure that both strings ends with '\0'
+//   env doesn't contain the = at the end, we compare with len + 1
+//   because we need to ensure that both strings end with '\0'
 const char	*get_env(char *env, t_list *var_list)
 {
 	t_content_var	*envp_content;
@@ -41,7 +41,6 @@ static char	*ft_strjoin_free(char *s1, char *s2)
 	return (joined);
 }
 
-//TODO: check if I can don't use the size
 char	**convert_list_to_envp(t_list *var_list)
 {
 	int				i;
