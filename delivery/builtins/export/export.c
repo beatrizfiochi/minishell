758b32940021dd40692d855d8d80bd4fddfd1ef3
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2025-07-09 20:24:40 +0100

    starting the var assign at export builtin

--- delivery/builtins/export/export.c
+++ delivery/builtins/export/export.c
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:43:42 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/07/08 18:46:53 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/07/09 20:14:18 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,15 +51,15 @@ static int	export_existent_variable(t_shell *shell, char *variable)
 			ft_strlen(variable)) == 0)
 			{
 				var_content->is_exported = true;
-				return (0);
+				return (EXIT_SUCCESS);
 			}
 		}
 		tmp_list = tmp_list->next;
 	}
-	return (1);
+	return (EXIT_FAILURE);
 }
 
-void	create_empty_variable_node(t_shell *shell, char *var)
+static int	create_empty_variable_node(t_shell *shell, char *var)
 {
 	t_list	*new_node;
 
@@ -67,33 +67,80 @@ void	create_empty_variable_node(t_shell *shell, char *var)
 	{
 		new_node = create_var_node(var, NULL, true);
 		if (new_node == NULL)
-			return ;
+			return (1);
 		ft_lstadd_back(&shell->variable_list, new_node);
 	}
 	else
+	{
 		ft_fprintf(STDERR_FILENO, "Minishell: export: `%s': not a valid identifier\n", var);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	assign_or_change_value_variable(t_shell *shell, char *var)
+{
+	t_list			*new_node;
+	char			*var_value;
+	int				i;
+
+	var_value = NULL;
+	i = 0;
+	while (var[i] != '=')
+		i++;
+	var[i] = '\0';
+	var_value = &var[i + 1];
+	if (check_and_replace_var(shell->variable_list, var,
+		var_value) == true)
+		return (EXIT_SUCCESS);
+	else if(is_valid_name(var, ft_strlen(var)) == 0)
+	{
+		if (is_valid_name(var, ft_strlen(var)) == 0)
+		{
+			new_node = create_var_node(var, var_value, true);
+			if (new_node == NULL)
+				return (1);
+			ft_lstadd_back(&shell->variable_list, new_node);
+		}
+		else
+		{
+			ft_fprintf(STDERR_FILENO, "Minishell: export: `%s=%s': not a valid identifier\n", var, var_value);
+			return (EXIT_FAILURE);
+		}
+	}
+	return (EXIT_SUCCESS);
 }
 
 int	export(int argc, char *argv[], t_shell *shell)
 {
 	int		i;
-	char	*var;
+	int		j;
+	int		ret;
 
 	i = 1;
+	j = 0;
+	ret = EXIT_SUCCESS;
 	if (i == argc)
 		print_export(shell);
 	else if (i < argc)
 	{
 		while (i < argc)
 		{
-			var = malloc(sizeof (char) * (ft_strlen(argv[i])) + 1);
-			if (var == NULL)
-				return (1);
-			ft_strlcpy(var, argv[i], (ft_strlen(argv[i]) + 1));
-			if (export_existent_variable(shell, var) == 1)
-				create_empty_variable_node(shell, var);
+			while (argv[i][j] != '\0')
+			{
+				if (argv[i][j] == '=')
+				{
+					if (assign_or_change_value_variable(shell, argv[i]) == EXIT_FAILURE)
+						ret = (EXIT_FAILURE);
+					break;
+				}
+				j++;
+			}
+			if (export_existent_variable(shell, argv[i]) == EXIT_FAILURE)
+				if (create_empty_variable_node(shell, argv[i]) == EXIT_FAILURE)
+					ret = EXIT_FAILURE;
 			i++;
 		}
 	}
-	return (0);
+	return (ret);
 }
