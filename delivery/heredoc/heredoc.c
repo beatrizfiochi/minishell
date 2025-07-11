a21aaa048d71e0a3d16a713d9f1bd29bcb1e80ff
Daniel Junho <djunho@gmail.com>
2025-07-11 20:20:02 +0100

    Fix Norm

--- delivery/heredoc/heredoc.c
+++ delivery/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: djunho <djunho@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 18:31:10 by djunho            #+#    #+#             */
-/*   Updated: 2025/07/11 09:43:50 by djunho           ###   ########.fr       */
+/*   Updated: 2025/07/11 20:19:31 by djunho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,41 @@ static char	*get_heredoc_file_name(void)
 	return (file_name);
 }
 
-static int	process_heredoc(t_shell *shell, t_content_node *content, char **eof)
+static int	heredoc_read_document(t_shell *shell, int fd, char *eof)
 {
-	int			line_start = 0;
-	char		*file_name;
-	char		*line;
-	int			fd;
-	int			ret;
+	int		line_start;
+	char	*line;
+	int		ret;
 
 	ret = EXIT_SUCCESS;
 	line_start = shell->nlines;
+	line = sh_read_line(shell, COLOR_MAGENTA HEREDOC_PROMPT COLOR_RESET);
+	while ((line != NULL) && (ft_strncmp(line, eof, ft_strlen(eof) + 1) != 0))
+	{
+		ret = EXIT_FAILURE;
+		if (write(fd, line, ft_strlen(line)) == -1)
+			break ;
+		ret = EXIT_SUCCESS;
+		write(fd, "\n", 1);
+		free(line);
+		line = sh_read_line(shell, COLOR_MAGENTA HEREDOC_PROMPT COLOR_RESET);
+	}
+	if ((line == NULL) && (g_signal == SIGINT))
+		ret = EXIT_SIGNAL_BASE + g_signal;
+	else if (line == NULL)
+		ft_fprintf(STDERR_FILENO, "warning: here-document at line %d delimited"
+			"by end-of-file (wanted `%s')\n", line_start, eof);
+	else
+		free(line);
+	return (ret);
+}
+
+static int	process_heredoc(t_shell *shell, t_content_node *content, char **eof)
+{
+	char		*file_name;
+	int			fd;
+	int			ret;
+
 	file_name = get_heredoc_file_name();
 	fd = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd == -1)
@@ -58,30 +83,13 @@ static int	process_heredoc(t_shell *shell, t_content_node *content, char **eof)
 		return (EXIT_FAILURE);
 	}
 	heredoc_signals();
-	line = sh_read_line(shell, COLOR_MAGENTA HEREDOC_PROMPT COLOR_RESET);
-	while ((line != NULL) && (ft_strncmp(line, *eof, ft_strlen(*eof) + 1) != 0))
-	{
-		if (write(fd, line, ft_strlen(line)) == -1)
-		{
-			free(file_name);
-			free(line);
-			return (EXIT_FAILURE);
-		}
-		write(fd, "\n", 1);
-		free(line);
-		line = sh_read_line(shell, COLOR_MAGENTA HEREDOC_PROMPT COLOR_RESET);
-	}
-	if (line == NULL)
-	{
-		if (g_signal == SIGINT)
-			ret = EXIT_SIGNAL_BASE + g_signal;
-		else
-			ft_fprintf(STDERR_FILENO, "warning: here-document at line %d delimited by end-of-file (wanted `%s')\n",
-				line_start, *eof);
-	}
-	else
-		free(line);
+	ret = heredoc_read_document(shell, fd, *eof);
 	heredoc_ignore_signals();
+	if (ret != EXIT_SUCCESS)
+	{
+		free(file_name);
+		return (ret);
+	}
 	close(fd);
 	content->op = OP_RD_INPUT;
 	free(*eof);
@@ -89,12 +97,11 @@ static int	process_heredoc(t_shell *shell, t_content_node *content, char **eof)
 	return (ret);
 }
 
-
 int	check_tree_for_heredoc(t_shell *shell, t_btnode *node)
 {
 	int				ret;
-	t_content_node *content;
-	t_content_node *right_content;
+	t_content_node	*content;
+	t_content_node	*right_content;
 
 	ret = EXIT_SUCCESS;
 	if (btree_is_leaf(node) || (node->right == NULL))
@@ -103,12 +110,10 @@ int	check_tree_for_heredoc(t_shell *shell, t_btnode *node)
 	right_content = (t_content_node *)node->right->content;
 	if (content->op == OP_HEREDOC)
 		ret = process_heredoc(shell, content,
-			(char **)(&right_content->cmd.tokens->content)
-					);
+				(char **)(&right_content->cmd.tokens->content));
 	else if (ret == EXIT_SUCCESS)
 		ret = check_tree_for_heredoc(shell, node->right);
 	if ((ret == EXIT_SUCCESS) && (node->left != NULL))
 		ret = check_tree_for_heredoc(shell, node->left);
 	return (ret);
 }
-
