3fbe637c053851cea1eb56e70358acc7a846eafb
Daniel Junho <djunho@gmail.com>
2025-07-15 10:59:32 +0100

    Improve builtin cd
    - Handle the PWD adn OLDPWD variables
    - Fix the issue when getcwd return null
    ```
    cd /tmp
    mkdir t1
    cd t1
    mkdir t1
    cd t1
    rm -rf ../../t1
    cd ..
    ```
    - Although not exactly the same, don't crash =)
    Example of bash
    ```
    djunho@djunho-work:/$ cd tmp/t1/t1
    djunho@djunho-work:/tmp/t1/t1$ env | grep PWD
    OLDPWD=/
    djunho@djunho-work:/tmp/t1/t1$ unset PWD
    djunho@djunho-work:/tmp/t1/t1$ env | grep PWD
    OLDPWD=/
    djunho@djunho-work:/tmp/t1/t1$ cd ..
    djunho@djunho-work:/tmp/t1$ env | grep PWD
    OLDPWD=
    djunho@djunho-work:/tmp/t1$ cd ..
    djunho@djunho-work:/tmp$ env | grep PWD
    OLDPWD=/tmp/t1
    djunho@djunho-work:/tmp$ cd t1
    djunho@djunho-work:/tmp/t1$ cd t1
    djunho@djunho-work:/tmp/t1/t1$ env | grep PWD
    OLDPWD=/tmp/t1
    djunho@djunho-work:/tmp/t1/t1$ unset PWD
    djunho@djunho-work:/tmp/t1/t1$ env | grep PWD
    OLDPWD=/tmp/t1
    djunho@djunho-work:/tmp/t1/t1$ cd ..
    djunho@djunho-work:/tmp/t1$ env | grep PWD
    OLDPWD=
    djunho@djunho-work:/tmp/t1$ unset PWD
    djunho@djunho-work:/tmp/t1$ env | grep PWD
    OLDPWD=
    djunho@djunho-work:/tmp/t1$ cd ..
    djunho@djunho-work:/tmp$ env | grep PWD
    OLDPWD=
    ```
    
    For some reason, bash only set OLDPWD to "" when the uset is frashly
    unsetted.

--- delivery/builtins/cd/cd.c
+++ delivery/builtins/cd/cd.c
@@ -16,6 +16,7 @@
 #include "../../libft/libft.h"
 #include "../../execute/env_utils.h"
 #include "../../minishell.h"
+#include "../../variables/variables.h"
 
 static void	p_error(const char *first, const char *second)
 {
@@ -28,45 +29,80 @@ static void	p_error(const char *first, const char *second)
 	free(final);
 }
 
-static char	*build_path(char *path)
+static char	*get_home(t_shell *shell)
 {
-	char	*aux;
-	char	*cwd;
-	char	*new_path;
+	char		*new_path;
 
-	if (path[0] == '/')
-		return (ft_strdup(path));
-	cwd = getcwd(NULL, 0);
-	aux = ft_strjoin(cwd, "/");
-	free(cwd);
-	new_path = ft_strjoin(aux, path);
-	free(aux);
-	return (new_path);
+	new_path = (char *)get_env("HOME", shell->variable_list);
+	if (new_path != NULL)
+		return (new_path);
+	ft_fprintf(STDERR_FILENO, "cd: HOME not set\n");
+	return (NULL);
+}
+
+static char	*concat_path(const char *path, const char *new_path)
+{
+	char	*new_pwd;
+	char	*new_pwd2;
+
+	if ((path == NULL) || (new_path == NULL))
+		return (NULL);
+	if (new_path[0] == '/')
+	{
+		new_pwd = ft_strdup(new_path);
+		if (new_pwd == NULL)
+			perror("cd: malloc failed");
+		return (new_pwd);
+	}
+	new_pwd = NULL;
+	new_pwd2 = ft_strjoin(path, "/");
+	if (new_pwd2 != NULL)
+		new_pwd = ft_strjoin(new_pwd2, new_path);
+	if ((new_pwd == NULL) || (new_pwd2 == NULL))
+		perror("cd: malloc failed");
+	if (new_pwd2 != NULL)
+		free(new_pwd2);
+	return (new_pwd);
+}
+
+static void	update_variables(t_shell *shell, const char *path)
+{
+	char			*new_path;
+	char			*var_pwd;
+
+	var_pwd = (char *)get_env("PWD", shell->variable_list);
+	if (var_pwd != NULL)
+		check_and_replace_var(shell->variable_list, "OLDPWD", var_pwd, NULL);
+	else
+		check_and_replace_var(shell->variable_list, "OLDPWD", "", NULL);
+	new_path = getcwd(NULL, 0);
+	if (new_path == NULL)
+	{
+		perror("cd: getcwd failed");
+		new_path = concat_path(var_pwd, path);
+	}
+	if (new_path != NULL)
+	{
+		check_and_replace_var(shell->variable_list, "PWD", new_path, NULL);
+		free(new_path);
+	}
 }
 
 int	cd(int argc, char *argv[], t_shell *shell)
 {
 	char		*new_path;
-	const char	*arg;
 	int			ret;
 
-	arg = argv[1];
 	if (argc == 1)
-	{
-		arg = (char *)get_env("HOME", shell->variable_list);
-		if (arg == NULL)
-		{
-			printf("bash: cd: HOME not set");
-			return (EXIT_FAILURE);
-		}
-		new_path = (char *)arg;
-	}
+		new_path = get_home(shell);
 	else
-		new_path = build_path(argv[1]);
+		new_path = argv[1];
+	if (new_path == NULL)
+		return (1);
 	ret = chdir(new_path);
 	if (ret != 0)
-		p_error("minishell: cd: ", arg);
-	if (argc != 1)
-		free(new_path);
+		p_error("minishell: cd: ", new_path);
+	else
+		update_variables(shell, new_path);
 	return (ret != 0);
 }
