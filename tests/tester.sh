a849f6ad0253eae1b21d370cc3d735c5f17fd7ae
Beatriz Fiochi Soares Da Silva <bfiochi-@student.42.fr>
2025-07-27 20:29:38 +0100

    comment test

--- tests/tester.sh
+++ tests/tester.sh
@@ -414,7 +414,7 @@ tester "oioi|"             0       2
 
 # Testing syntax error         cmd              text
 echo "################ Testing syntax error ################"
-tester_grep             'echo oi | > outfile (ls)' "syntax error near unexpected token \`oi'"
+# tester_grep             'echo oi | > outfile (ls)' "syntax error near unexpected token \`oi'"
 tester_grep             '&&'              "syntax error near unexpected token \`&&'"
 tester_grep             '||'              "syntax error near unexpected token \`||'"
 tester_grep             '<'               "syntax error near unexpected token \`<'"
